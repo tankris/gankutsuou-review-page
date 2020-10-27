19180b3928b9ce95ea039f09e5d2cb2c47c6dbcc
Tanishq Krishnan <kronorak@outlook.com>
2020-10-27 23:45:45 +0530

    Add image resource and Update index.html & main.js
    This commit includes the following:
       - Modifications to index.html include:
         - Some grammatical corrections,
         - Added a new button and also the path of the image has been
         simplied for usage in the JS code (in main.js)
       - The main.js adds the following new functionalities to the site:
         - Change the poster upon clicking it
         - Accept user's name, store it in the browser's local storage
         and display it as part of the review (this was already present,
         however the ability to accept user input is new)
         - Includes a Button to change the user's name upon clicking it
       - A new image resource has been added to enable the new JS
       functionality (of changing the poster)

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,68 @@
-const myHeading = document.querySelector(".user");
-myHeading.textContent = "Tanishq";
+//Change the image upon clicking it
+
+//Select the img tag
+let myImage = document.querySelector("img");
+
+//Upon clicking it, call the anonymous function
+myImage.onclick = function () {
+  //Extract the src attribute
+  let mySrc = myImage.getAttribute("src");
+
+  //If the 1st poster link is being href'd, then change it
+  //and vice-versa
+  //Note that the alt attribute is also being changed
+  if (mySrc === "images/gankutsuou-poster.jpg") {
+    myImage.setAttribute("src", "images/gankutsuou-poster-2.jpg");
+    myImage.setAttribute(
+      "alt",
+      "Gankutsuou Poster: A long haired anti-hero (below) and (on top) our protagonist"
+    );
+  } else {
+    myImage.setAttribute("src", "images/gankutsuou-poster.jpg");
+    myImage.setAttribute(
+      "alt",
+      "Gankutsuou Poster: A long haired anti-hero caresing a naive boy"
+    );
+  }
+};
+
+//Greet user in the review section
+
+//Select the user class associated tag
+let nameSetter = document.querySelector(".user");
+
+//Select the button tag
+let changeUser = document.querySelector("button");
+
+function setGrettingName() {
+  //Prompt for input
+  let myName = prompt("Please enter your name.");
+
+  //If the name has been entered,
+  //create a kay-value pair in the local storage of the browser
+  //and insert the string into the user class associated tag
+  if (myName) {
+    localStorage.setItem("name", myName);
+    nameSetter.innerHTML = myName;
+  }
+
+  //If the name hasn't been entered or the prompt was cancelled,
+  //alert the user and re-call the function
+  else {
+    alert("Enter a Valid Name");
+    setGrettingName();
+  }
+}
+
+//If there isn't an entry for the key=name in the browser's local storage, then call the above function
+//Else use the value associated with the name key and insert in the user class associated tag
+if (localStorage.getItem("name") === null) {
+  setGrettingName();
+} else {
+  nameSetter.textContent = localStorage.getItem("name");
+}
+
+//Trigger function call upon clicking the "Change User" button, to change the value of name in the local browser's storage
+changeUser.onclick = function () {
+  setGrettingName();
+};
